79e683d04f9273b8f8629dd790c6e9e743ff3d7e
Pedro Medeiros <108948373+xPedro-Medeiros@users.noreply.github.com>
2022-11-25 09:06:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,14 +5,12 @@ const btnClose = document.getElementById('btn-close')
 btnOpen.addEventListener('click', () => {
     if (mobileMenu.classList.contains("mobile-menu") == true) {
         mobileMenu.classList.replace("mobile-menu", "slide")
-        mobileMenu.setAttribute("data-slide", "true")
         btnOpen.style.display = 'none'
     }
 })
 
 btnClose.addEventListener('click', () => {
     if (mobileMenu.classList.contains("slide") == true) {
-        mobileMenu.setAttribute("data-slide", "false")
         mobileMenu.classList.replace("slide", "mobile-menu")
         btnOpen.style.display = 'block'
 
